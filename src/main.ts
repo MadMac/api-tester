7a9513576e88af6105404a624c332e01195a4baa
Markus Hällfors <markus@hallfors.com>
2025-09-17 18:24:24 +0300

    Start adding shadcn

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 import { createApp } from "vue";
-import "./styles.css";
+import "./index.css";
 import App from "./App.vue";
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
-	components,
-	directives,
-	theme: {
-		defaultTheme: 'dark'
-	}
-})
-
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+});
 
 createApp(App).use(vuetify).mount("#app");
